b86e98954291c2124b1620c422d97d3da7ef7ca6
trev1400 <trevfix@gmail.com>
2022-11-09 16:40:44 -0500

    Finished studio

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
+import { Row } from "react-bootstrap";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+	item.image = process.env.PUBLIC_URL + "/" + item.image;
 });
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+	const [cart, setCart] = useState({});
+	const [cartTotal, setCartTotal] = useState(0);
 
-  return (
-    <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+	return (
+		<div className="App p-4">
+			<h1 className="my-3">Trevor's Bakery</h1>
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+			<div className="d-flex justify-content-between gap-4 w-100">
+				<Row xs={1} sm={2} md={3} xlg={4} className="w-75 g-3">
+					{bakeryData.map((item, index) => (
+						<BakeryItem
+							key={index}
+							item={item}
+							cart={cart}
+							setCart={setCart}
+							cartTotal={cartTotal}
+							setCartTotal={setCartTotal}
+						/>
+					))}
+				</Row>
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
-    </div>
-  );
+				<div className="d-flex flex-column w-25 px-4 gap-2">
+					<h2>Cart</h2>
+					<div
+						className="flex-column gap-2 p-3 cart"
+						style={
+							Object.keys(cart).length > 0
+								? { display: "flex" }
+								: { display: "none" }
+						}
+					>
+						{Object.entries(cart).map(([id, item]) => (
+							<div
+								key={id}
+								className="w-100"
+							>{`${item.count}x ${item.name} ($${item.price})`}</div>
+						))}
+					</div>
+					{cartTotal ? (
+						<span className="fw-bold fs-5 ms-1">
+							{`Total: $${cartTotal}`}
+						</span>
+					) : (
+						<span>Add something to your cart!</span>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,42 @@
-// TODO: create a component that displays a single bakery item
+import "../App.css";
+import { Card, Col, Button } from "react-bootstrap";
+
+function BakeryItem(props) {
+	const { item, cart, setCart, cartTotal, setCartTotal } = props;
+
+	const onAddToCart = () => {
+		const newCart = {
+			...cart,
+			[item.id]:
+				item.id in cart
+					? { ...item, count: cart[item.id]["count"] + 1 }
+					: { ...item, count: 1 },
+		};
+		setCart(newCart);
+		setCartTotal(Math.round((cartTotal + item.price) * 100) / 100);
+	};
+
+	return (
+		<Col>
+			<Card>
+				<Card.Img src={item.image} />
+				<Card.Body className="d-flex flex-column gap-2">
+					<Card.Title>{item.name}</Card.Title>
+					<Card.Text>{item.description}</Card.Text>
+					<div className="d-flex justify-content-between align-items-center mt-auto">
+						<span>{`$${item.price}`}</span>
+						<Button
+							variant="light"
+							className="btn-md ms-auto"
+							onClick={onAddToCart}
+						>
+							Add to Cart
+						</Button>
+					</div>
+				</Card.Body>
+			</Card>
+		</Col>
+	);
+}
+
+export default BakeryItem;
